8081be91c3b63e839526f4282573794fcc3f43a9
Aditya Singh Bisht <115521200+AdityaSinghBisht3000@users.noreply.github.com>
2023-12-23 13:08:31 +0000

    for loop done

--- try1.js
+++ try1.js
@@ -1,5 +1,32 @@
-let Name="Adi"
-let clas="3rd Yr"
-let stud="yes"
-console.log("JaishreeRam")
-console.table([Name],[clas],[stud])
+// let Name="Adi"
+// let clas="3rd Yr"
+// let stud="yes"
+// // console.log("JaishreeRam")
+// // console.table([Name],[clas],[stud])
+
+// let a=10;
+// let b=100;
+
+// // console.log(+(a>b))
+
+// ( 
+//     (usrname)=>{
+//         console.log("hi",`${usrname} hello\n`)
+//     }
+//  )("Aditya");
+
+// const obj={
+//     name: "adi",
+//     class: "Btech"
+// }
+
+// // console.log(obj["name"])
+
+// for (const key in obj) {
+//     console.log(`${key} :- ${obj[key]}`)
+// }
+
+const arr=["adi","ayu","monu","sonu"]
+arr.forEach( (val)=> {
+    console.log(val)
+} )
